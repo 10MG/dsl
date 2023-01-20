e0d291af3dcc29624b4916d0677d825138f81be9
June <wjzhao@aliyun.com>
2023-01-20 10:09:02 +0800

    ToStringParamsConverter支持formatter

--- src/main/java/cn/tenmg/dsl/converter/ToStringParamsConverter.java
+++ src/main/java/cn/tenmg/dsl/converter/ToStringParamsConverter.java
@@ -1,5 +1,11 @@
 package cn.tenmg.dsl.converter;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import cn.tenmg.dsl.utils.DateUtils;
+import cn.tenmg.dsl.utils.DecimalUtils;
+
 /**
  * 将参数转换为 {@code java.lang.String} 类型的转换器
  * 
@@ -7,11 +13,25 @@ package cn.tenmg.dsl.converter;
  * 
  * @since 1.3.0
  */
-public class ToStringParamsConverter extends AbstractParamsConverter<String> {
+public class ToStringParamsConverter extends FormatableParamsConverter<String> {
 
 	@Override
 	public String convert(Object value) {
-		return value == null ? null : value.toString();
+		if (value == null) {
+			return null;
+		} else {
+			String formatter = getFormatter();
+			if (formatter != null) {
+				if (value instanceof Number) {
+					return DecimalUtils.format(value, formatter);
+				} else if (value instanceof Date) {
+					return DateUtils.format(value, formatter);
+				} else if (value instanceof Calendar) {
+					return DateUtils.format(((Calendar) value).getTime(), formatter);
+				}
+			}
+		}
+		return value.toString();
 	}
 
 }
