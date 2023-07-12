fd26d98ff9fd36de1aeda668ed43a9238cb64cfe
June <wjzhao@aliyun.com>
2023-07-12 18:28:38 +0800

    增加两个指定值类型的构建Map的方法

--- src/main/java/cn/tenmg/dsl/utils/MapUtils.java
+++ src/main/java/cn/tenmg/dsl/utils/MapUtils.java
@@ -14,16 +14,32 @@ import java.util.Set;
  */
 public abstract class MapUtils {
 
+	/**
+	 * 判断 {@code Map} 对象是否为 {@code null} 或者不包含任何元素。
+	 * 
+	 * @param m
+	 *            {@code Map} 对象
+	 * @return 如果 {@code Map} 对象 {@code m} 为 {@code null} 或者非 {@code null}
+	 *         但不包含任何元素，则返回 {@code true}；否则，返回 {@code false}。
+	 */
 	public static boolean isEmpty(Map<?, ?> m) {
 		return m == null || m.isEmpty();
 	}
 
+	/**
+	 * 判断 {@code Map} 对象是否非 {@code null} 且至少包含一个元素。
+	 * 
+	 * @param m
+	 *            {@code Map} 对象
+	 * @return 如果 {@code Map} 对象非 {@code null} 且至少包含一个元素，则返回 {@code true}；否则，返回
+	 *         {@code false}。
+	 */
 	public static boolean isNotEmpty(Map<?, ?> m) {
 		return m != null && !m.isEmpty();
 	}
 
 	/**
-	 * 使用键集合移除指定查找表中的元素
+	 * 使用键集合移除指定查找表中的元素。
 	 * 
 	 * @param map
 	 *            指定查找表
@@ -36,44 +52,200 @@ public abstract class MapUtils {
 		}
 	}
 
+	/**
+	 * 创建一个 {@code HashMap<K, V>} 对象。
+	 * 
+	 * @return 新建的 {@code HashMap<K, V>} 对象。
+	 */
+	public static <K, V> HashMap<K, V> newHashMap() {
+		return new HashMap<K, V>();
+	}
+
+	/**
+	 * 创建一个初始容量为 {@code initialCapacity} 的 {@code HashMap<K, V>} 对象。
+	 * 
+	 * @return 新建的 {@code HashMap<K, V>} 对象。
+	 */
+	public static <K, V> HashMap<K, V> newHashMap(int initialCapacity) {
+		return new HashMap<K, V>(initialCapacity);
+	}
+
+	/**
+	 * 创建一个含有一个键为 {@code key}、值为 {@code value} 元素的 {@code HashMap<K, V>} 对象 。
+	 * 
+	 * @param key
+	 *            键
+	 * @param value
+	 *            值
+	 * @return 新建的 {@code HashMap<K, V>} 对象。
+	 */
 	public static <K, V> HashMap<K, V> newHashMap(K key, V value) {
 		HashMap<K, V> map = new HashMap<K, V>();
 		map.put(key, value);
 		return map;
 	}
-	
+
+	/**
+	 * 将一个 {@code Map<K, V>} 对象转换为一个 {@code HashMap<K, V>} 对象。
+	 * 
+	 * @param map
+	 *            {@code Map<K, V>} 对象
+	 * @return 新建的 {@code HashMap<K, V>} 对象。
+	 */
 	public static <K, V> HashMap<K, V> toHashMap(Map<K, V> map) {
 		HashMap<K, V> copy = new HashMap<K, V>(map.size());
 		copy.putAll(map);
 		return copy;
 	}
 
+	/**
+	 * 创建一个初始容量为 {@code initialCapacity}，且含有一个键为 {@code key}、值为 {@code value} 元素的
+	 * {@code HashMap<K, V>} 对象。
+	 * 
+	 * @param initialCapacity
+	 *            初始容量
+	 * @param key
+	 *            键
+	 * @param value
+	 *            值
+	 * @return 新建的 {@code HashMap<K, V>} 对象。
+	 */
 	public static <K, V> HashMap<K, V> newHashMap(int initialCapacity, K key, V value) {
 		HashMap<K, V> map = new HashMap<K, V>(initialCapacity);
 		map.put(key, value);
 		return map;
 	}
 
-	public static <K, V> MapBuilder<K, V> newMapBuilder(Map<K, V> map) {
-		return new MapBuilder<K, V>(map);
-	}
-
-	public static <K, V> MapBuilder<K, V> toHashMapBuilder(Map<K, V> map) {
-		Map<K, V> copy = new HashMap<K, V>(map.size());
-		copy.putAll(map);
-		return new MapBuilder<K, V>(copy);
-	}
-
-	public static <K, V> MapBuilder<K, V> newHashMapBuilder(K key, V value) {
-		return new MapBuilder<K, V>(newHashMap(key, value));
-	}
-
-	public static <K, V> MapBuilder<K, V> newHashMapBuilder(int initialCapacity, K key, V value) {
-		return new MapBuilder<K, V>(newHashMap(initialCapacity, key, value));
+	/**
+	 * 创建一个含有 {@code map} 所有元素的 {@code MapBuilder<M, K, V>} 对象。
+	 * 
+	 * @param map
+	 *            {@code Map<K, V>} 对象
+	 * @return 新建的 {@code MapBuilder<M, K, V>} 对象。
+	 */
+	public static <M extends Map<K, V>, K, V> MapBuilder<M, K, V> newMapBuilder(M map) {
+		return new MapBuilder<M, K, V>(map);
 	}
 
 	/**
-	 * Map构建器
+	 * 创建一个 {@code MapBuilder<HashMap<K, V>, K, V>} 含有 {@code map} 所有元素的
+	 * {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 * 
+	 * @param map
+	 *            查找表对象
+	 * @return 新建的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> toHashMapBuilder(Map<K, V> map) {
+		HashMap<K, V> copy = new HashMap<K, V>(map.size());
+		copy.putAll(map);
+		return new MapBuilder<HashMap<K, V>, K, V>(copy);
+	}
+
+	/**
+	 * 创建一个含有一个键为 {@code key}、值为 {@code value} 元素的
+	 * {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 * 
+	 * @param key
+	 *            键
+	 * @param value
+	 *            值
+	 * @return 新建的哈希查找表构建器，即 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> newHashMapBuilder(K key, V value) {
+		return new MapBuilder<HashMap<K, V>, K, V>(newHashMap(key, value));
+	}
+
+	/**
+	 * 创建一个含有一个键为 {@code key}、值为 {@code value} 元素，且初始容量为 {@code initialCapacity} 的
+	 * {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 * 
+	 * @param initialCapacity
+	 *            初始容量
+	 * @param key
+	 *            键
+	 * @param value
+	 *            值
+	 * @return 新建的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> newHashMapBuilder(int initialCapacity, K key, V value) {
+		return new MapBuilder<HashMap<K, V>, K, V>(newHashMap(initialCapacity, key, value));
+	}
+
+	/**
+	 * 创建一个 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 * 
+	 * @return 新建的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> newHashMapBuilder() {
+		return new MapBuilder<HashMap<K, V>, K, V>(new HashMap<K, V>());
+	}
+
+	/**
+	 * 创建一个初始容量为 {@code initialCapacity} 的 {@code MapBuilder<HashMap<K, V>, K, V>}
+	 * 对象。
+	 * 
+	 * @return 新建的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> newHashMapBuilder(int initialCapacity) {
+		return new MapBuilder<HashMap<K, V>, K, V>(new HashMap<K, V>(initialCapacity));
+	}
+
+	/**
+	 * 创建一个键类型为 {@code keyType} 的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 * 
+	 * @param keyType
+	 *            键类型
+	 * @return 新建的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> newHashMapBuilder(Class<K> keyType) {
+		return new MapBuilder<HashMap<K, V>, K, V>(new HashMap<K, V>());
+	}
+
+	/**
+	 * 创建一个键类型为 {@code keyType}，初始容量为 {@code initialCapacity} 的
+	 * {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 * 
+	 * @param keyType
+	 *            键类型
+	 * @param initialCapacity
+	 *            初始容量
+	 * @return 新建的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> newHashMapBuilder(Class<K> keyType, int initialCapacity) {
+		return new MapBuilder<HashMap<K, V>, K, V>(new HashMap<K, V>(initialCapacity));
+	}
+
+	/**
+	 * 创建一个键类型为 {@code keyType}、值类型为 {@code valueType} 的
+	 * {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 * 
+	 * @param keyType
+	 *            键类型
+	 * @param valueType
+	 *            值类型
+	 * @return 新建的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> newHashMapBuilder(Class<K> keyType, Class<V> valueType) {
+		return new MapBuilder<HashMap<K, V>, K, V>(new HashMap<K, V>());
+	}
+
+	/**
+	 * 创建一个键类型为 {@code keyType}、值类型为 {@code valueType}，初始容量为 {@code initialCapacity}
+	 * 的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 * 
+	 * @param keyType
+	 *            键类型
+	 * @param initialCapacity
+	 *            初始容量
+	 * @return 新建的 {@code MapBuilder<HashMap<K, V>, K, V>} 对象。
+	 */
+	public static <K, V> MapBuilder<HashMap<K, V>, K, V> newHashMapBuilder(Class<K> keyType, Class<V> valueType,
+			int initialCapacity) {
+		return new MapBuilder<HashMap<K, V>, K, V>(new HashMap<K, V>(initialCapacity));
+	}
+
+	/**
+	 * 查找表构建器
 	 * 
 	 * @author June wjzhao@aliyun.com
 	 * 
@@ -84,58 +256,60 @@ public abstract class MapUtils {
 	 * @param <V>
 	 *            值的类型
 	 */
-	public static class MapBuilder<K, V> {
+	public static class MapBuilder<M extends Map<K, V>, K, V> {
 
-		private final Map<K, V> map;
+		private final M map;
 
-		public MapBuilder(Map<K, V> map) {
+		public MapBuilder(M map) {
 			super();
 			this.map = map;
 		}
 
 		/**
-		 * 将键、值存入希查找表中
+		 * 如果查找表非 {@code null}，则将查找表中的所有元素存入查找表构建器中
 		 * 
 		 * @param map
-		 *            指定查找表
-		 * @return MapBuilder对象
+		 *            查找表
+		 * @return 查找表构建器
 		 */
-		public MapBuilder<K, V> putAll(Map<K, V> map) {
+		public MapBuilder<M, K, V> putAll(M map) {
 			if (map != null) {
-				map.putAll(map);
+				this.map.putAll(map);
 			}
 			return this;
 		}
 
 		/**
-		 * 将指定查找表中的元素全部放入哈希表中
+		 * 向查找表构建器存入一个键为 {@code key}，值为 {@code value} 的元素
 		 * 
 		 * @param key
 		 *            键
 		 * @param value
 		 *            值
-		 * @return MapBuilder对象
+		 * @return 查找表构建器
 		 */
-		public MapBuilder<K, V> put(K key, V value) {
+		public MapBuilder<M, K, V> put(K key, V value) {
 			map.put(key, value);
 			return this;
 		}
 
 		/**
-		 * 构建并返回Map对象
+		 * 构建并返回查找表对象
 		 * 
-		 * @return 构建的Map对象
+		 * @return 构建的查找表对象
 		 */
-		public Map<K, V> build() {
+		public M build() {
 			return map;
 		}
 
 		/**
-		 * 构建并返回Map对象
+		 * 将
 		 * 
-		 * @return 构建的Map对象
+		 * @param key
+		 * @param value
+		 * @return
 		 */
-		public Map<K, V> build(K key, V value) {
+		public M build(K key, V value) {
 			map.put(key, value);
 			return map;
 		}
@@ -145,9 +319,9 @@ public abstract class MapUtils {
 		 * 
 		 * @return 返回构建的Map对象
 		 */
-		public Map<K, V> build(Map<K, V> map) {
-			map.putAll(map);
-			return map;
+		public M build(Map<K, V> map) {
+			this.map.putAll(map);
+			return this.map;
 		}
 
 	}
