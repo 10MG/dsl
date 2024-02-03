5f23c13ac8811f270695ee4f4bd26c4fd4d58f36
June <wjzhao@aliyun.com>
2024-02-03 13:16:36 +0800

    DateUtils和DecimalUtils增加缓存，提升效率

--- src/main/java/cn/tenmg/dsl/utils/DateUtils.java
+++ src/main/java/cn/tenmg/dsl/utils/DateUtils.java
@@ -1,8 +1,10 @@
 package cn.tenmg.dsl.utils;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
 
 import cn.tenmg.dsl.exception.DateFormatException;
 import cn.tenmg.dsl.exception.DateParseException;
@@ -15,34 +17,32 @@ import cn.tenmg.dsl.exception.DateParseException;
  * @since 1.3.0
  */
 public abstract class DateUtils {
+
+	private static final ConcurrentHashMap<String, DateFormat> FORMATTERS = new ConcurrentHashMap<String, DateFormat>();
+
 	/**
 	 * 根据模板将制定对象格式化为日期字符串
 	 * 
-	 * @param obj
-	 *            指定对象
-	 * @param pattern
-	 *            模板
+	 * @param obj     指定对象
+	 * @param pattern 模板
 	 * @return 日期字符串
 	 */
 	public static String format(Object obj, String pattern) {
-		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-		return sdf.format(obj);
+		return formatter(pattern).format(obj);
 	}
 
 	/**
 	 * 根据模板将指定对象转换为日期对象
 	 * 
-	 * @param obj
-	 *            指定对象
-	 * @param pattern
-	 *            模板
+	 * @param obj     指定对象
+	 * @param pattern 模板
 	 * @return 日期对象
 	 */
 	public static Date parse(Object obj, String pattern) {
-		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		DateFormat formatter = formatter(pattern);
 		if (obj instanceof String) {
 			try {
-				return sdf.parse((String) obj);
+				return formatter.parse((String) obj);
 			} catch (Exception e) {
 				throw new DateParseException(StringUtils.concat("An exception occurred when using the pattern: ",
 						pattern, " to parse the String: ", obj, " to Date"), e);
@@ -50,26 +50,24 @@ public abstract class DateUtils {
 		}
 		String str;
 		try {
-			str = sdf.format(obj);
+			str = formatter.format(obj);
 		} catch (Exception e) {
 			throw new DateFormatException(StringUtils.concat("An exception occurred when using the pattern: ", pattern,
 					" to format the Object: ", obj, " to String"), e);
 		}
 		try {
-			return sdf.parse(str);
+			return formatter.parse(str);
 		} catch (Exception e) {
 			throw new DateParseException(StringUtils.concat("An exception occurred when using the pattern: ", pattern,
 					" to parse the String: ", str, " to Date"), e);
 		}
 	}
-	
+
 	/**
 	 * 向指定日期加上或减去指定天数，正数为加，负数为减
 	 * 
-	 * @param date
-	 *            指定日期
-	 * @param days
-	 *            指定天数
+	 * @param date 指定日期
+	 * @param days 指定天数
 	 * @return 返回新的日期
 	 */
 	public static Date addDays(Date date, int days) {
@@ -79,10 +77,8 @@ public abstract class DateUtils {
 	/**
 	 * 向指定日期加上或减去指定月数，正数为加，负数为减
 	 * 
-	 * @param date
-	 *            指定日期
-	 * @param months
-	 *            指定月数
+	 * @param date   指定日期
+	 * @param months 指定月数
 	 * @return 返回新的日期
 	 */
 	public static Date addMonths(Date date, int months) {
@@ -92,10 +88,8 @@ public abstract class DateUtils {
 	/**
 	 * 向指定日期加上或减去指定年数，正数为加，负数为减
 	 * 
-	 * @param date
-	 *            指定日期
-	 * @param years
-	 *            指定年数
+	 * @param date  指定日期
+	 * @param years 指定年数
 	 * @return 返回新的日期
 	 */
 	public static Date addYears(Date date, int years) {
@@ -105,10 +99,8 @@ public abstract class DateUtils {
 	/**
 	 * 截断日期
 	 * 
-	 * @param date
-	 *            日期
-	 * @param fmt
-	 *            截断格式 SECOND/MINUTE/HOUR/DAY/MONTH/YEAR，不区分大小写
+	 * @param date 日期
+	 * @param fmt  截断格式 SECOND/MINUTE/HOUR/DAY/MONTH/YEAR，不区分大小写
 	 * @return 返回新的日期
 	 */
 	public static Date trunc(Date date, String fmt) {
@@ -144,12 +136,9 @@ public abstract class DateUtils {
 	/**
 	 * 向指定日期加上指定单位的指定量
 	 * 
-	 * @param date
-	 *            指定日期
-	 * @param field
-	 *            指定单位
-	 * @param amount
-	 *            指定量
+	 * @param date   指定日期
+	 * @param field  指定单位
+	 * @param amount 指定量
 	 * @return 返回新的日期
 	 */
 	public static Date add(Date date, int field, int amount) {
@@ -157,5 +146,14 @@ public abstract class DateUtils {
 		cal.setTime(date);
 		cal.add(field, amount);
 		return cal.getTime();
+	}
+
+	private static DateFormat formatter(String pattern) {
+		DateFormat formatter = FORMATTERS.get(pattern);
+		if (formatter == null) {
+			formatter = new SimpleDateFormat(pattern);
+			FORMATTERS.put(pattern, formatter);
+		}
+		return formatter;
 	}
 }
--- src/main/java/cn/tenmg/dsl/utils/DecimalUtils.java
+++ src/main/java/cn/tenmg/dsl/utils/DecimalUtils.java
@@ -1,6 +1,8 @@
 package cn.tenmg.dsl.utils;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.concurrent.ConcurrentHashMap;
 
 import cn.tenmg.dsl.exception.NumberParseException;
 
@@ -12,35 +14,33 @@ import cn.tenmg.dsl.exception.NumberParseException;
  * @since 1.3.0
  */
 public abstract class DecimalUtils {
+
+	private static final ConcurrentHashMap<String, NumberFormat> FORMATTERS = new ConcurrentHashMap<String, NumberFormat>();
+
 	/**
 	 * 根据模板将指定对象格式化数字字符串
 	 * 
-	 * @param obj
-	 *            指定对象
-	 * @param pattern
-	 *            模板
+	 * @param obj     指定对象
+	 * @param pattern 模板
 	 * @return 数字字符串
 	 */
 	public static String format(Object obj, String pattern) {
-		DecimalFormat df = new DecimalFormat(pattern);
-		return df.format(obj);
+		return formatter(pattern).format(obj);
 	}
 
 	/**
 	 * 根据模板将指定对象转换为数字对象
 	 * 
-	 * @param obj
-	 *            指定对象
-	 * @param pattern
-	 *            模板
+	 * @param obj     指定对象
+	 * @param pattern 模板
 	 * @return 数字对象
 	 */
 
 	public static Number parse(Object obj, String pattern) {
-		DecimalFormat df = new DecimalFormat(pattern);
+		NumberFormat formatter = formatter(pattern);
 		if (obj instanceof String) {
 			try {
-				return df.parse((String) obj);
+				return formatter.parse((String) obj);
 			} catch (Exception e) {
 				throw new NumberParseException(StringUtils.concat("An exception occurred when using the pattern: ",
 						pattern, " to parse the String: ", obj, " to Number"), e);
@@ -48,16 +48,25 @@ public abstract class DecimalUtils {
 		}
 		String str;
 		try {
-			str = df.format(obj);
+			str = formatter.format(obj);
 		} catch (Exception e) {
 			throw new NumberParseException(StringUtils.concat("An exception occurred when using the pattern: ", pattern,
 					" to format the Object: ", obj, " to String"), e);
 		}
 		try {
-			return df.parse(str);
+			return formatter.parse(str);
 		} catch (Exception e) {
 			throw new NumberParseException(StringUtils.concat("An exception occurred when using the pattern: ", pattern,
 					" to parse the String: ", str, " to Number"), e);
 		}
+	}
+
+	private static NumberFormat formatter(String pattern) {
+		NumberFormat formatter = FORMATTERS.get(pattern);
+		if (formatter == null) {
+			formatter = new DecimalFormat(pattern);
+			FORMATTERS.put(pattern, formatter);
+		}
+		return formatter;
 	}
 }
