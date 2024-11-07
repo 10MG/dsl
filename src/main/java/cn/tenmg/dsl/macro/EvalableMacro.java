688222316e2ae6c0d15912c66e75e9287f0852b3
June <wjzhao@aliyun.com>
2024-11-07 17:20:36 +0800

    优化异常处理代码

--- src/main/java/cn/tenmg/dsl/macro/EvalableMacro.java
+++ src/main/java/cn/tenmg/dsl/macro/EvalableMacro.java
@@ -31,7 +31,8 @@ public abstract class EvalableMacro implements Macro {
 			try {
 				evalEngine = (EvalEngine) Class.forName(className).getConstructor().newInstance();
 			} catch (Exception e) {
-				evalEngine = new JavaScriptEngine();
+				throw new RuntimeException(String.format("Cannot instantiated {} using default constructor", className),
+						e);
 			}
 		}
 		return evalEngine;
@@ -54,8 +55,7 @@ public abstract class EvalableMacro implements Macro {
 	/**
 	 * 将带参数的字符串代码转换为 {@code ScriptEngine} 可执行的代码
 	 * 
-	 * @param code
-	 *            带参数的字符串代码
+	 * @param code 带参数的字符串代码
 	 * @return 可执行的代码
 	 */
 	protected static String toEvalable(String code) {
@@ -100,16 +100,11 @@ public abstract class EvalableMacro implements Macro {
 	/**
 	 * 执行宏并解析DSL动态片段。如果返回结果为{@code true}，则DSL解析立即终止，并以当前宏解析DSL动态片段的结果为DSL解析的最终结果；否则，将当前宏解析的DSL片段结果拼接到DSL的主解析结果中，并继续后续解析。
 	 * 
-	 * @param evalEngine
-	 *            代码执行引擎
-	 * @param context
-	 *            DSL上下文
-	 * @param attributes
-	 *            属性表。由当前层已运行的宏所存储，供本层后续执行的宏使用
-	 * @param logic
-	 *            宏逻辑代码
-	 * @param dslf
-	 *            DSL片段构建器
+	 * @param evalEngine 代码执行引擎
+	 * @param context    DSL上下文
+	 * @param attributes 属性表。由当前层已运行的宏所存储，供本层后续执行的宏使用
+	 * @param logic      宏逻辑代码
+	 * @param dslf       DSL片段构建器
 	 * @return 如果返回结果为{@code true}，则DSL解析立即终止，并以当前宏解析DSL动态片段的结果为DSL解析的最终结果；否则，将当前宏解析的DSL片段结果拼接到DSL的主解析结果中，并继续后续解析。
 	 * @throws Exception
 	 */
